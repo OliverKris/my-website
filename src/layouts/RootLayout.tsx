c08e11c18a4021d38820caf3d2f0b559b2d066fb
Oliver Krisetya <okrisetya@gmail.com>
2025-12-25 01:51:27 -0500

    Added socials in the footer, updated brightness change

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar.tsx";
 import styles from "./RootLayout.module.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function RootLayout() {
     return (
@@ -16,6 +17,16 @@ export default function RootLayout() {
             </main>
 
             <footer className={styles.footer}>
+                <section className={styles.socialSection}>
+                    <div className={styles.socialIcons}>
+                        <a href="https://linkedin.com/in/okrisetya" aria-label="LinkedIn">
+                            <FaLinkedin />
+                        </a>
+                        <a href="https://github.com/OliverKris" aria-label="GitHub">
+                            <FaGithub />
+                        </a>
+                    </div>
+                </section>
                 <p className={styles.footerText}>© {new Date().getFullYear()} Oliver Krisetya</p>
             </footer>
         </div>
