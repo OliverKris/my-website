86fad06ec4f59c888bab116ba486c3c6b0cbcaca
Oliver Krisetya <okrisetya@gmail.com>
2026-05-28 11:09:01 -0400

    Added individual project folder structure, began work on hero for formality project

---
+++ src/pages/projects/formality_analysis/index.tsx
@@ -0,0 +1,11 @@
+import Hero from './sections/Hero';
+import Intro from './sections/Intro'
+
+export default function Formality(){
+    return(
+        <div>
+            <Hero />
+            <Intro />
+        </div>
+    )
+}
---
+++ src/pages/projects/formality_analysis/sections/Intro.tsx
@@ -0,0 +1,18 @@
+import '../formality.css'
+
+export default function Intro() {
+    return (
+        <section style={{
+            minHeight: '100vh',
+            background: 'var(--f-green-dark)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontFamily: 'var(--f-font-sans)',
+            position: 'relative',
+            overflow: 'hidden',
+        }}>
+
+        </section>
+    )
+}
